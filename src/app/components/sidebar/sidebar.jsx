23ecf04580eaadf1aa2c2548a1c0b4d775425480
furkan akin <furkanakinmfa@gmail.com>
2023-08-21 00:54:17 +0300

    create project and add pages

---
+++ src/app/components/sidebar/sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './sidebar.module.css'
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>sidebar</div>
+  )
+}
+
+export default Sidebar
